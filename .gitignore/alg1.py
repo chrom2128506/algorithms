01b7636a3f941353b13cb5e60ddf4f7a83c4848b
chrom2128506 <36818053+chrom2128506@users.noreply.github.com>
2019-02-10 00:11:27 +0200

    Create alg1.py

---
+++ .gitignore/alg1.py
@@ -0,0 +1,17 @@
+f = open('zerosones.txt', 'r')
+s = f.read()
+#print(s)
+#print(len(s))
+length = 0
+for i in range(len(s)): 
+    if s[i] == '1':
+        for j in range(i, len(s)):
+            if s[j] == '0':
+                length = j - i
+                if(prev_length > length):
+                    length = prev_length
+                break
+    prev_length = length
+    
+print(length)
+f.close()
