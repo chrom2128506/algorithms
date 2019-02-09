1e4df4ff3ec2eba469b0301a04a4122222fe91ba
chrom2128506 <36818053+chrom2128506@users.noreply.github.com>
2019-02-10 00:40:24 +0200

    Update alg2.py

--- alg2.py
+++ alg2.py
@@ -3,13 +3,18 @@
 
 # In[ ]:
 
-
 num = int(input())
 ar = []
-for i in range(1, num+1):
-    if (num % i == 0):
-        ar.append(i)
-        
-print(ar)
+
+if num > 0: 
+    for i in range(1, num+1):
+        if (num % i == 0):
+            ar.append(i)
+    print(ar)
+elif num<0: 
+    for i in range(-1, num-1, -1):
+        if (num % i == 0):
+            ar.append(i)
+    print(ar)
     
 
